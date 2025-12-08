3e76856d2c0ced2ed03f4fc60bd18a37f5955866
Congyan (Cruise) Song <115286861+cruisesong7@users.noreply.github.com>
2025-12-08 14:50:14 -0500

    version set to v4.21.0 to align with formal_ramsey

--- lakefile.lean
+++ lakefile.lean
@@ -3,10 +3,11 @@ open Lake DSL
 
 package «GraphMaker»
 
-lean_lib "GraphMaker" where
+lean_lib «GraphMaker» where
 
 @[default_target]
 lean_exe "graphmaker" where
   root := `Main
 
-require proofwidgets from git "https://github.com/leanprover-community/ProofWidgets4"@"v0.0.75"
+require proofwidgets from git "https://github.com/leanprover-community/ProofWidgets4"@"v0.0.64"
+require formal_ramsey from git "https://github.com/cruisesong7/formal_ramsey"@"c1ec6fc"
