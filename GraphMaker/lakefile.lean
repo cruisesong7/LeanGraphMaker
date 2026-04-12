d4e53ed4f37ef1e16e0c8e21ae9928004753976d
Congyan (Cruise) Song <115286861+cruisesong7@users.noreply.github.com>
2026-04-12 19:01:53 -0400

    bump to 4.28.0

--- GraphMaker/lakefile.lean
+++ GraphMaker/lakefile.lean
@@ -5,5 +5,5 @@ package «GraphMaker»
 
 lean_lib «GraphMaker» where
 
-require proofwidgets from git "https://github.com/leanprover-community/ProofWidgets4"@"v0.0.64"
-require formal_ramsey from git "https://github.com/cruisesong7/formal_ramsey"@"7e9b963"
+require proofwidgets from git "https://github.com/leanprover-community/ProofWidgets4"@"v0.0.87"
+require formal_ramsey from git "https://github.com/cruisesong7/formal_ramsey"@"9ecbb4f"
