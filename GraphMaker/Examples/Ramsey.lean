c816ea352f87c204ce462da226e248112a8d8879
Congyan (Cruise) Song <115286861+cruisesong7@users.noreply.github.com>
2026-04-12 21:36:30 -0400

    Ramsey example completed

--- GraphMaker/Examples/Ramsey.lean
+++ GraphMaker/Examples/Ramsey.lean
@@ -1,19 +1,73 @@
 import FormalRamsey.RamseyGraphs
 import GraphMaker.DrawGraph
 
-theorem R33 : ¬(RamseyGraphProp 5 3 3) := by
+namespace RamseyGraphExamples
+
+/-!
+# Lower Bounds for Small Ramsey Numbers
+
+This file contains proofs establishing the lower bounds of various small Ramsey numbers
+by providing explicit graph counterexamples.
+
+In general, to prove a lower bound such as $R(s, t) > n$, we must demonstrate the
+existence of a graph of order $n$ that lacks both a clique of size $s$ and an
+independent set of size $t$. This is formalized by showing that a specific graph
+does not satisfy `RamseyGraphProp n s t`.
+
+### Example: $R(2,3) > 2$
+To show that the Ramsey number $R(2,3)$ is strictly greater than 2, we find a
+counterexample graph of order 2 that does not have the `RamseyGraphProp`—meaning
+it has no clique of 2 nor an independent set of 3.
+
+To aid in visualizing these constructions, the concrete counterexamples can be
+inspected directly in the Infoview by invoking the `draw_graph` command.
+-/
+
+
+theorem R23 : ¬(RamseyGraphProp 2 2 3) := by
   simp only [RamseyGraphProp, not_forall]
-  let G := readG6 "DeS"
+  --draw_graph
+  let G := readG6 "A?"
   use G
   simp [not_or]
   apply And.intro
   · intros S
     rw [← SimpleGraph.mem_cliqueFinset_iff]
-    have cliqueFree : (readG6 "DeS").cliqueFinset 3 = Finset.empty := by native_decide
-    rw [cliqueFree]
+    rw [show G.cliqueFinset 2 = Finset.empty by native_decide]
     exact Finset.notMem_empty S
   · intros T
     rw [← SimpleGraph.mem_indepSetFinset_iff]
-    have cliqueFree : (readG6 "GhdGKC").indepSetFinset 3 = Finset.empty := by native_decide
-    rw [cliqueFree]
+    rw [show G.indepSetFinset 3 = Finset.empty by native_decide]
+    exact Finset.notMem_empty T
+
+theorem R33 : ¬(RamseyGraphProp 5 3 3) := by
+  simp only [RamseyGraphProp, not_forall]
+  --draw_graph
+  let G := readG6 "Dhc"
+  use G
+  simp [not_or]
+  apply And.intro
+  · intros S
+    rw [← SimpleGraph.mem_cliqueFinset_iff]
+    rw [show G.cliqueFinset 3 = Finset.empty by native_decide]
+    exact Finset.notMem_empty S
+  · intros T
+    rw [← SimpleGraph.mem_indepSetFinset_iff]
+    rw [show G.indepSetFinset 3 = Finset.empty by native_decide]
+    exact Finset.notMem_empty T
+
+theorem R34 : ¬(RamseyGraphProp 8 3 4) := by
+  simp only [RamseyGraphProp, not_forall]
+  --draw_graph
+  let G := readG6 "GhdHKc"
+  use G
+  simp [not_or]
+  apply And.intro
+  · intros S
+    rw [← SimpleGraph.mem_cliqueFinset_iff]
+    rw [show G.cliqueFinset 3 = Finset.empty by native_decide]
+    exact Finset.notMem_empty S
+  · intros T
+    rw [← SimpleGraph.mem_indepSetFinset_iff]
+    rw [show G.indepSetFinset 4 = Finset.empty by native_decide]
     exact Finset.notMem_empty T
